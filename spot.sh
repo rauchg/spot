aff933f3f4751d176a44eaa7a41a89f69596922f
Subramaniyam Raizada <sub@subraizada.com>
2019-06-14 17:20:12 -0500

    fix bugs when searching for multiple words or for spaces

--- spot.sh
+++ spot.sh
@@ -118,7 +118,7 @@ if [[ "$1" =~ / ]]; then
 fi
 
 # check for empty search
-if [ -z "$@" ]; then
+if [[ -z "$@" ]]; then
   echo "(no search term. \`spot -h\` for usage)"
   exit 1
 fi
@@ -160,7 +160,7 @@ fi
 
 # run search
 eval "find $dir $findopt -type f $exclude -print0" \
-  | GREP_COLOR="1;33;40" xargs -0 grep "${grepopt[@]}" -e "$@" \
+  | GREP_COLOR="1;33;40" xargs -0 grep "${grepopt[@]}" -e "`echo \"$@\"`" \
   | sed "s/^\([^:]*:\)\(.*\)/  \\
 $cyan\1$reset  \\
 \2 /" \
